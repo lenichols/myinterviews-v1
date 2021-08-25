95138a5fcbb97b61c68d017564d25bf3a8392f35
Carol Sanders <carol@carolsanders.org>
2021-08-24 21:31:39 -0700

    Updated login.js login.css footer.js - added copyright statement added login.png to Read.md

--- src/components/Login1.js
+++ src/components/Login1.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect, useContext } from "react";
+// import Button from './Button'
 import { Link, useHistory } from 'react-router-dom';
 import Dashboard from './Dashboard';
 import Footer from './Footer';
@@ -6,7 +7,7 @@ import './Login.css';
 import { signInWithGoogle, auth } from "../firebase/firebase";
 // import { LoginContext } from "./LoginContext";
 import { useLogin } from "./LoginContext";
-import reactDom from "react-dom";
+
 
 const SignIn = () => {
     //const [displayName, setDisplayName] = useState();
@@ -15,23 +16,23 @@ const SignIn = () => {
     let isLoggedIn  = useLogin();
 
     return (
-   <div className="row h-100" id="login">
-    {/* <div className="container"> */}
-         <div className="login google-auth">
-           <div className='container'>
-           <h2><span>{'>'}</span> My Interview <span>{'<'}</span></h2>
-           <h1>Diary</h1>
-           <h4>Document + Journal + Reflect</h4>             
-           {/* <div className="login google-auth"> */}
-            <button type="button" onClick={isLoggedIn} className=" btn btn-lg btn-block btn-black d-inline float-right">
+      <div className="row h-100" id="login">
+          <div className="container col-lg-12 offset-md-2 my-auto">
+             {/* <header> */}
+                  <h2><span></span> {'>'} My Interview  {'<'} <span></span></h2>
+                  <h1>Diary</h1>
+                  <h4>Document + Journal + Reflect</h4>
+            {/*}  </header> */}
+              <br/>
+              <div className="form-group text-right">
+              <button id="signInButtn" type="button" onClick={isLoggedIn} className="btn btn-lg btn-block btn-dark d-inline float-right">
             Sign in with Google
           </button>
-          <p><a href="">Create an Account</a></p>
           </div>
-         </div>
-       {/* </div> */}
-       <Footer />
-   </div>
-)
+              <Footer />
+          </div>
+      </div>
+  )
 };
+
 export default SignIn;
